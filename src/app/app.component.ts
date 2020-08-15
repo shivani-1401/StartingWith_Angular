c2a0ce896110f70b19e990e8608d02a683a634ab
shivani97 <shivani.agrawal76@gmail.com>
2020-08-15 21:49:00 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string ;
-  employees : Employee[];
+  title: string;
+  employees: Employee[];
+  constructor() {
+    this.title = "Registration details";
+    this.employees = [new Employee(1, 'Raghav', 'Singh', 'A/22 Gaziabad', 'raghavsingh@gmail.com', 'raghav@123', 'Male', '22-Aug-1990'),
+    new Employee(2, 'Sujit', 'Kumar', 'Kanpur', 'sujitkumar@rediffmail.com', 'sujitkumar@123', 'Male', '12-Nov-1981'),
+    new Employee(3, 'Payel', 'Dutta', 'Kolkata', 'payeldutta@gmail.com', 'payel@123', 'Female', '12-Mar-1985'),
+    new Employee(4, 'Murali', 'Naidu', 'Chennai', 'muralinaidu@gmail.com', 'muralinaidu@123', 'Male', '05-Jun-1989')
 
-  constructor()
-  {
-     
-    
+    ];
+
   }
 }
--- src/app/employee.ts
+++ src/app/employee.ts
@@ -1,5 +1,5 @@
-export class Employee{
-
-    
+export class Employee {
+    constructor(public id: number, public firstname: string, public lastname: string, public address: string, public email: string, public password: string, public gender: string, public date_of_birth: string) {
+    }
 
 }
